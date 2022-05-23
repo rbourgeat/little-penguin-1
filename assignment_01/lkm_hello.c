7db80b3e453fcd68e52a9009a652aec2eaa3c8f8
Raphael bourgeat <cosmosraph@gmail.com>
2022-05-23 16:31:35 +0200

    assignment 01 done

---
+++ assignment_01/lkm_hello.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbourgea <rbourgea@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 16:07:48 by rbourgea          #+#    #+#             */
+/*   Updated: 2022/05/23 16:11:04 by rbourgea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Raphaël Bourgeat <rbourgea@student.42.fr>");
+MODULE_DESCRIPTION("A simple Hello World Linux module.");
+MODULE_VERSION("1.0");
+
+static int __init lkm_hello_init(void)
+{
+    printk(KERN_INFO "Hello, World !\n");
+    return 0;
+}
+
+static void __exit lkm_hello_exit(void)
+{
+    printk(KERN_INFO "Cleaning up module.\n");
+}
+
+module_init(lkm_hello_init);
+module_exit(lkm_hello_exit);
