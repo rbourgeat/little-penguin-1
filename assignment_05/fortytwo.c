115835118cf122a2d3ea32e3aeb0c818369c74f0
krolhm <raphaelb1@icloud.com>
2022-05-26 15:32:35 +0200

    assignment 05 fix

--- assignment_05/fortytwo.c
+++ assignment_05/fortytwo.c
@@ -6,11 +6,12 @@
 /*   By: rbourgea <rbourgea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 18:58:36 by rbourgea          #+#    #+#             */
-/*   Updated: 2022/05/26 14:24:33 by rbourgea         ###   ########.fr       */
+/*   Updated: 2022/05/26 15:24:47 by rbourgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Sources: https://embetronicx.com/tutorials/linux/device-drivers/misc-device-driver/
+// && https://gist.github.com/chuckleberryfinn/e50a938083ec355563efc9da1f2fa904
 
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -27,27 +28,40 @@ MODULE_DESCRIPTION("A simple Linux module.");
 MODULE_VERSION("1.0");
 
 static ssize_t etx_misc_write(struct file *file, const char __user *buf,
-               size_t len, loff_t *ppos)
+		size_t len, loff_t *ppos)
 {
 	pr_info("EtX misc device write\n");
 
-	return len; 
+	return simple_read_from_buffer(buf, count, f_pos, LOGIN,
+					strlen(LOGIN));
 }
  
 static ssize_t etx_misc_read(struct file *filp, char __user *buf,
-                    size_t count, loff_t *f_pos)
+		size_t count, loff_t *f_pos)
 {
 	pr_info("EtX misc device read\n");
 
-	return 0;
+	char specified_msg[count];
+	ssize_t retval = -EINVAL;
+
+	if (count != strlen(LOGIN))
+                return retval;
+
+	retval = simple_write_to_buffer(specified_msg, count, f_pos, buf,
+					count);
+	if (retval < 0)
+		return retval;
+
+	retval = strncmp(LOGIN, specified_msg, count) ? -EINVAL : count;
+	return retval;
 }
 
 static const struct file_operations fops = {
 	.owner          = THIS_MODULE,
-	.write          = fortytwo_misc_write,
-	.read           = fortytwo_misc_read,
-	// .open           = fortytwo_misc_open,
-	// .release        = fortytwo_misc_close,
+	.write          = etx_misc_write,
+	.read           = etx_misc_read,
+	// .open           = etx_misc_open,
+	// .release        = etx_misc_close,
 	// .llseek         = no_llseek,
 };
 
@@ -59,6 +73,8 @@ struct miscdevice fortytwo_misc_device = {
 
 static int __init misc_init(void)
 {
+	pr_debug("Hello there\nMy minor number is: %i\n",
+                        misc_example.minor);
 	int error;
 
 	error = misc_register(&fortytwo_misc_device);
