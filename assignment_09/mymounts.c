1daf15c9ecfe9d6098fb519d876061765d27ae8f
krolhm <raphaelb1@icloud.com>
2022-05-29 17:40:13 +0200

    assignment_09 test

--- assignment_09/mymounts.c
+++ assignment_09/mymounts.c
@@ -6,7 +6,7 @@
 /*   By: rbourgea <rbourgea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 12:13:55 by rbourgea          #+#    #+#             */
-/*   Updated: 2022/05/29 17:36:32 by rbourgea         ###   ########.fr       */
+/*   Updated: 2022/05/29 17:38:09 by rbourgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,11 +62,12 @@ static ssize_t mymounts_read(struct file *file, char __user *buf, size_t count,
 		return -ENOMEM;
 	// get all mountpoints / checking all dentries of a system
 	list_for_each_entry(mnt, &current->nsproxy->mnt_ns->list, mnt_list) {
-	// if (strcmp(mnt->mnt_devname, "none") == 0)
-	// 	continue;
+	if (strcmp(mnt->mnt_devname, "none") == 0)
+		continue;
 	if (strcmp(mnt->mnt_devname, "/dev/sda1") == 0)
 		continue;
-	pr_info("[%s]", mnt->mnt_devname);
+	// pr_info("[%s]", mnt->mnt_devname);
+	pr_info("[%s]", mnt->mnt_mountpoint);
 	memset(path, 0, sizeof(path));
 	// 3 cases: /dev/root, / and others
 	if (strcmp(mnt->mnt_devname, "/dev/root") == 0) {
