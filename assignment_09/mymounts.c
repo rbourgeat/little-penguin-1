267fecfa89f5fd9b97096192c4f3aa4baad3d06f
krolhm <raphaelb1@icloud.com>
2022-05-29 17:35:58 +0200

    assignment_09 test

--- assignment_09/mymounts.c
+++ assignment_09/mymounts.c
@@ -6,7 +6,7 @@
 /*   By: rbourgea <rbourgea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 12:13:55 by rbourgea          #+#    #+#             */
-/*   Updated: 2022/05/29 17:34:46 by rbourgea         ###   ########.fr       */
+/*   Updated: 2022/05/29 17:35:49 by rbourgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ static ssize_t mymounts_read(struct file *file, char __user *buf, size_t count,
 	list_for_each_entry(mnt, &current->nsproxy->mnt_ns->list, mnt_list) {
 	// if (strcmp(mnt->mnt_devname, "none") == 0)
 	// 	continue;
-	if (strcmp(mnt->mnt_devname, "/dev/root") == 0)
+	if (strcmp(mnt->mnt_devname, "/dev/sda1") == 0)
 		continue;
 	// pr_info("[%s]", mnt->mnt_devname);
 	memset(path, 0, sizeof(path));
