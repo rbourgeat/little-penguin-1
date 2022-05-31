05a10fa8c45160720e24d9c5222facc8a1f45d3f
rbourgeat <raphaelb1@icloud.com>
2022-05-31 15:01:16 +0200

    fix norm 08

--- assignment_08/following_file.c
+++ assignment_08/following_file.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-only
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -5,17 +6,14 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 
-// Dont have a license, LOL
-MODULE_LICENSE("LICENSE");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
 
-static ssize_t myfd_read(struct file *fp, char __user *user, size_t size,
-			loff_t *offs);
-static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
-			loff_t *offs);
+static ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs);
+static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs);
 
-static struct file_operations myfd_fops = {
+static const struct file_operations myfd_fops = {
 	.owner = THIS_MODULE,
 	.read = &myfd_read,
 	.write = &myfd_write
@@ -44,8 +42,7 @@ static void __exit myfd_cleanup(void)
 	misc_deregister(&myfd_device);
 }
 
-ssize_t myfd_read(struct file *fp, char __user *user, size_t size,
-			loff_t *offs)
+ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
 {
 	size_t t, i;
 	// char *tmp2;
@@ -53,19 +50,18 @@ ssize_t myfd_read(struct file *fp, char __user *user, size_t size,
 	int ret;
 
 	/***************
-	* Malloc like a boss
-	***************/
+	 * Malloc like a boss
+	 ***************/
 	// tmp2 = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_KERNEL);
 	// tmp = tmp2;
 	// for (t = strlen(str) - 1, i = 0; t >= 0; t--, i++) {
-	// 	tmp[i] = str[t];
+	//	tmp[i] = str[t];
 	// }
 	tmp = kmalloc(PAGE_SIZE, GFP_KERNEL);
 	if (!tmp)
 		return -ENOMEM;
-	for (t = (strlen(str) - 1), i = 0; i < strlen(str); t--, i++) {
+	for (t = (strlen(str) - 1), i = 0; i < strlen(str); t--, i++)
 		tmp[i] = str[t];
-	}
 	tmp[i] = 0x0;
 	// return simple_read_from_buffer(user, size, offs, tmp, i);
 	ret = simple_read_from_buffer(user, size, offs, tmp, strlen(tmp));
@@ -73,8 +69,7 @@ ssize_t myfd_read(struct file *fp, char __user *user, size_t size,
 	return ret;
 }
 
-ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
-			loff_t *offs)
+ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs)
 {
 	ssize_t res;
 
@@ -91,3 +86,4 @@ ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
 
 module_init(myfd_init);
 module_exit(myfd_cleanup);
+
